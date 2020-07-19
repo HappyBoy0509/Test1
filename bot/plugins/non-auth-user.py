25eda54c9b9501eba255f8bc9b9338f0d9db28fc
HappyBoy0509 <65845452+HappyBoy0509@users.noreply.github.com>
2020-07-19 13:12:24 +0530

    Update non-auth-user.py

--- bot/plugins/non-auth-user.py
+++ bot/plugins/non-auth-user.py
@@ -1,13 +1,1 @@
-from pyrogram import Filters
 
-from ..utubebot import UtubeBot
-from ..config import Config
-
-
-@UtubeBot.on_message(
-    Filters.private 
-    & Filters.incoming
-    & ~Filters.user(Config.AUTH_USERS)
-)
-async def _non_auth_usr_msg(c, m):
-    await m.delete(True)
